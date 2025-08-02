ddc61b5f425437644bde0a14d967efcaa42f17f9
Ernesto Ruiz <eruiz@wbapps.com>
2025-08-02 13:37:06 -0600

    chore: add translation function on remarks in make_asset_revaluation_entry in asset_value_adjustment.py

--- erpnext/assets/doctype/asset_value_adjustment/asset_value_adjustment.py
+++ erpnext/assets/doctype/asset_value_adjustment/asset_value_adjustment.py
@@ -100,7 +100,9 @@ class AssetValueAdjustment(Document):
 		je.naming_series = depreciation_series
 		je.posting_date = self.date
 		je.company = self.company
-		je.remark = f"Revaluation Entry against {self.asset} worth {self.difference_amount}"
+		je.remark = _("Revaluation Entry against {0} worth {1}").format(
+			self.asset, self.difference_amount
+		)
 		je.finance_book = self.finance_book
 
 		entry_template = {
